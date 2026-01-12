a621b58d21f4fed6224b9498e43cb38c1dd1f6b9
Fabian Lehmann <90115995+Fxbixx@users.noreply.github.com>
2026-01-12 08:40:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,8 @@
-#include <SFML/Graphics.hpp>
+#include "Game.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Test");
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.display();
-    }
+    Game game;
+    game.run();
+    return 0;
 }
